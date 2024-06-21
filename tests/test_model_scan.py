cc9218459c1e51a47ac3fb1c12f95b914a82fd5b
Ashton Sidhu <asidhu@hiddenlayer.com>
2024-06-21 14:17:04 -0400

    Add enterprise support

--- tests/test_model_scan.py
+++ tests/test_model_scan.py
@@ -2,29 +2,41 @@ import pytest
 
 import model_scan
 
+params = [
+    ("https://api.hiddenlayer.ai"),
+    pytest.param(
+        "http://localhost:8000",
+    ),  # marks=pytest.mark.xfail)
+]
 
-def test_model_scan_local_file():
+
+@pytest.mark.parametrize("host", params)
+def test_model_scan_local_file(host):
     """Test that scanning a model doesn't break."""
-    model_scan.main(model_path="tests/models/example_model.xgb")
+    model_scan.main(model_path="tests/models/example_model.xgb", api_url=host)
 
 
-def test_model_scan_s3():
+@pytest.mark.parametrize("host", params)
+def test_model_scan_s3(host):
     """Test scanning a malicious model breaks."""
 
     with pytest.raises(SystemExit) as e:
         model_scan.main(
-            model_path="s3://hl-oss-integration-tests/example_models/malicious_torch.bin"
+            model_path="s3://hl-oss-integration-tests/example_models/malicious_torch.bin",
+            api_url=host,
         )
 
     assert e.value.code == 1
 
 
-def test_model_scan_azure():
+@pytest.mark.parametrize("host", params)
+def test_model_scan_azure(host):
     """Test scanning a malicious model on azure."""
 
     with pytest.raises(SystemExit) as e:
         model_scan.main(
-            model_path="https://dsdemomodelsstorage.blob.core.windows.net/azureml/malicious_model.bin"
+            model_path="https://dsdemomodelsstorage.blob.core.windows.net/azureml/malicious_model.bin",
+            api_url=host,
         )
 
     assert e.value.code == 1
