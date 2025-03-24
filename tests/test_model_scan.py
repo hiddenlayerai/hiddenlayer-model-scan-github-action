46dd9e634df65de3e2faae47db18b592af2c4139
Andrew Drummond <adrummond@hiddenlayer.com>
2025-03-24 12:17:39 -0600

    Update test to understand that community scan should exit on detection with error code

--- tests/test_model_scan.py
+++ tests/test_model_scan.py
@@ -197,13 +197,16 @@ def test_sarif_output_detections(host):
 def test_community_scan(host):
     """Test community Scan with HuggingFace ScanMe repo"""
 
-    model_scan.main(
-        model_path="ScanMe/Models",
-        api_url=host,
-        model_name="GHA Community Scan Test",
-        model_version="main",
-        community_scan="HUGGING_FACE",
-    )
+    with pytest.raises(SystemExit) as e:
+        model_scan.main(
+            model_path="ScanMe/Models",
+            api_url=host,
+            model_name="GHA Community Scan Test",
+            model_version="main",
+            community_scan="HUGGING_FACE",
+        )
+
+    assert e.value.code == 1
 
     with open("output.json", "r") as f:
         output = json.load(f)
